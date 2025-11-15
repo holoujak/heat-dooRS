043aead3e9972a3cd32addf403a1f8e6ebb2393c
holoujak <jakub.holoubek@gmail.com>
2025-11-15 21:10:02 +0100

    Blink LED every second

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,18 @@
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_stm32::Config;
+use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
     let config = Config::default();
-    let _p = embassy_stm32::init(config);
+    let p = embassy_stm32::init(config);
+    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
 
     loop {
+        led.toggle();
         info!("Hello World!");
         Timer::after_secs(1).await;
     }
