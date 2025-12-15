23762e77a6628791645e9b932371be123f9dcd83
holoujak <jakub.holoubek@gmail.com>
2025-12-15 23:17:52 +0100

    Measure NTC temperature

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,47 @@
 #![no_std]
 #![no_main]
 
+mod ntc;
+
+use crate::ntc::ntc;
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_stm32::Config;
 use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::peripherals::*;
+use embassy_stm32::{adc, bind_interrupts};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal};
+use embassy_time::Duration;
 use embassy_time::Timer;
+
+#[cfg(not(feature = "defmt"))]
+use panic_halt as _;
+
+#[cfg(feature = "defmt")]
 use {defmt_rtt as _, panic_probe as _};
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) -> ! {
-    let config = Config::default();
-    let p = embassy_stm32::init(config);
-    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
+bind_interrupts!(struct Irqs {
+    ADC1_2 => adc::InterruptHandler<ADC1>;
+});
 
+pub static SIGNAL_TEMPERATURE: Signal<CriticalSectionRawMutex, f32> = Signal::new();
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+
+    SIGNAL_TEMPERATURE.signal(0.0);
+
+    let led_pin = Output::new(p.PC13, Level::High, Speed::Low);
+
+    spawner.spawn(led_task(led_pin)).unwrap();
+    spawner.spawn(ntc(p.PA0, p.ADC1)).unwrap();
+}
+
+#[embassy_executor::task]
+async fn led_task(mut led_pin: Output<'static>) {
+    info!("Starting LED task");
     loop {
-        led.toggle();
-        info!("Hello World!");
-        Timer::after_secs(1).await;
+        led_pin.toggle();
+        Timer::after(Duration::from_secs(1)).await;
     }
 }
---
+++ src/ntc.rs
@@ -0,0 +1,62 @@
+use defmt::{info, trace};
+use embassy_executor::task;
+use embassy_stm32::Peri;
+use embassy_stm32::adc::{Adc, SampleTime};
+use embassy_stm32::peripherals::{ADC1, PA0};
+use embassy_time::Timer;
+use micromath::F32Ext;
+
+use crate::SIGNAL_TEMPERATURE;
+
+const ADC_MAX: f32 = 4095.0;
+const R_PULL: f32 = 10_000.0; // pull-down 10k
+const R_NTC_25: f32 = 10_000.0; // 10k @ 25°C
+const BETA: f32 = 3950.0;
+const T0: f32 = 298.15; // 25°C v K
+
+pub fn adc_to_temperature_c(adc: u16) -> f32 {
+    if adc == 0 || adc as f32 >= ADC_MAX {
+        return f32::NAN;
+    }
+
+    let adc_f = adc as f32;
+
+    // NTC to VCC, pull-down to GND
+    let r_ntc = R_PULL * (ADC_MAX - adc_f) / adc_f;
+
+    let inv_t = (1.0 / T0) + (1.0 / BETA) * (r_ntc / R_NTC_25).ln();
+
+    (1.0 / inv_t) - 273.15
+}
+
+#[task]
+pub async fn ntc(temp_pin: Peri<'static, PA0>, temp_adc: Peri<'static, ADC1>) {
+    let mut adc = Adc::new(temp_adc);
+    let mut pin = temp_pin;
+
+    let mut vrefint = adc.enable_vref();
+    adc.set_sample_time(SampleTime::CYCLES13_5);
+
+    let vrefint_sample = adc.read(&mut vrefint).await;
+    let convert_to_millivolts = |sample: u16| {
+        // From http://www.st.com/resource/en/datasheet/CD00161566.pdf
+        // 5.3.4 Embedded reference voltage
+        const VREFINT_MV: u32 = 1200; // mV
+
+        (u32::from(sample) * VREFINT_MV / u32::from(vrefint_sample)) as u16
+    };
+
+    loop {
+        let measured = adc.read(&mut pin).await;
+        trace!("--> {} - {} mV", measured, convert_to_millivolts(measured));
+
+        let temp_c = adc_to_temperature_c(measured);
+
+        if temp_c.is_normal() {
+            info!("Temperature: {}", temp_c);
+            SIGNAL_TEMPERATURE.signal(temp_c);
+        }
+
+        Timer::after_millis(1000).await;
+    }
+}
