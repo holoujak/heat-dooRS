c2cf4e137cc70fe9a8b3bbef76fc052eabfc37be
holoujak <jakub.holoubek@gmail.com>
2025-12-20 17:13:15 +0100

    Add motor control base on measured temperature

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![no_std]
 #![no_main]
 
+mod motor_control;
 mod ntc;
 
+use crate::motor_control::{MotorControl, MotorStatus, motor_control};
 use crate::ntc::ntc;
 use defmt::info;
 use embassy_executor::Spawner;
@@ -24,6 +26,7 @@ bind_interrupts!(struct Irqs {
 });
 
 pub static SIGNAL_TEMPERATURE: Signal<CriticalSectionRawMutex, f32> = Signal::new();
+pub static SIGNAL_MOTOR_STATUS: Signal<CriticalSectionRawMutex, MotorStatus> = Signal::new();
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -32,16 +35,34 @@ async fn main(spawner: Spawner) {
     SIGNAL_TEMPERATURE.signal(0.0);
 
     let led_pin = Output::new(p.PC13, Level::High, Speed::Low);
+    let motor_en_pin = Output::new(p.PA1, Level::Low, Speed::Low);
+    let motor_dir_pin = Output::new(p.PA2, Level::Low, Speed::Low);
 
+    let motor = MotorControl::new(motor_dir_pin, motor_en_pin);
     spawner.spawn(led_task(led_pin)).unwrap();
     spawner.spawn(ntc(p.PA0, p.ADC1)).unwrap();
+    spawner.spawn(motor_control(motor)).unwrap();
 }
 
 #[embassy_executor::task]
 async fn led_task(mut led_pin: Output<'static>) {
     info!("Starting LED task");
+    let mut current_status = MotorStatus::Off;
     loop {
-        led_pin.toggle();
-        Timer::after(Duration::from_secs(1)).await;
+        if let Some(status) = SIGNAL_MOTOR_STATUS.try_take() {
+            current_status = status;
+        }
+        match current_status {
+            MotorStatus::Closing => {
+                led_pin.set_low();
+            }
+            MotorStatus::Opening => {
+                led_pin.toggle();
+            }
+            MotorStatus::Off => {
+                led_pin.set_high();
+            }
+        }
+        Timer::after(Duration::from_millis(100)).await;
     }
 }
